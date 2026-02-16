44f24bc5144ce33410063901e3820348998d73c8
CJ Barreiro <cjbarreiro1@gmail.com>
2026-02-16 14:21:49 -0500

    added room exits and transition message

--- src/main.js
+++ src/main.js
@@ -1,37 +1,60 @@
 // Model
 const world = {
-    currentRoomID: "cockpit",
-    rooms: null
+    currentRoom: "controlRoom",
+    rooms: null,
+    transitionMessage: ""
 };
-
-let rooms = []
 
 // Controller
 const roomNameEl = document.querySelector("#room-name");
 const roomDescEl = document.querySelector("#room-desc");
+const roomExitsEl = document.querySelector("#room-exits");
+const messageAreaEl = document.querySelector("#message-area");
+
+roomExitsEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nextRoom = e.target.dataset.exit;
+    if (nextRoom && world.rooms[nextRoom]) {
+        world.currentRoom = nextRoom;
+        world.transitionMessage = `You are now in the ${world.rooms[world.currentRoom].name}`;
+        render();
+    }
+})
 
 async function init() {
     fetch("./db.json")
         .then(res => res.json())
         .then(data => {
-            rooms = data.rooms
-            world.rooms = data.rooms
-            render()
+            rooms = data.rooms;
+            world.rooms = data.rooms;
+            render();
         })
-        .catch(err => console.error(err))
+        .catch(err => console.error(err));
 }
 
+function createExit(exit) {
+    return `<a href="#" data-exit="${exit}" class="exit">${world.rooms[exit].name}</a>`;
+}
+
+function createMessage(message) {
+    return `<p class="message">${message}</p>`;
+}
 
 // View
 function render() {
-    const room = world.rooms[world.currentRoomID];
+    const room = world.rooms[world.currentRoom];
     if (!room) {
         return;
     }
 
+    exits = room.exits;
+
     roomNameEl.textContent = room.name;
     roomDescEl.textContent = room.description;
-
+    roomExitsEl.innerHTML = room.exits.map(exit => {
+        return createExit(exit);
+    }).join(" ");
+    messageAreaEl.innerHTML = createMessage(world.transitionMessage)
 }
 
 init()
